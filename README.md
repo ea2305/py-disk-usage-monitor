87b35dc82b7d9cb1c97b90e11cd11ee82a3e82a4
Elihu Cruz <eaca230596@gmail.com>
2022-12-20 14:20:55 -0600

    disk monitoring tool initialization
    - env configuration
    - development tools
    - first iteration of disk info extraction
    - influx connection, create or update instance
    - data insertion

---
+++ README.md
@@ -0,0 +1,12 @@
+# Disk backup monitor
+
+Implements a simple monitor to track the usage in a backup disk.
+
+### Setup
+> This application uses Influxdb to store the information, be sure the env variables are all set before run the application.
+
+Install dependencies: `pip3 install -r requirements.txt`
+
+### Run application
+
+`python3 main.py`
---
+++ main.py
@@ -0,0 +1,59 @@
+from dotenv import load_dotenv
+import psutil
+from influxdb import InfluxDBClient
+import os
+
+load_dotenv()
+
+# ENV variables
+CONFIG = {
+  'NUM_PATHS': os.environ.get('NUM_PATHS'),
+  'DISK_PATHS': os.environ.get('DISK_PATHS').split(','),
+}
+
+DB = {
+  'HOST': os.environ.get('DB_HOST'),
+  'PORT': int(os.environ.get('DB_PORT')),
+  'USER': os.environ.get('DB_USER'),
+  'PASSWORD': os.environ.get('DB_PASSWORD'),
+  'DATABASE': os.environ.get('DB_DATABASE')
+}
+
+# Setup
+influx = InfluxDBClient(DB['HOST'], DB['PORT'], DB['USER'], DB['PASSWORD'])
+dbs = influx.get_list_database()
+if DB['DATABASE'] not in dbs:
+  influx.create_database(DB['DATABASE'])
+
+influx.switch_database(DB['DATABASE'])
+
+def get_data():
+  # Get the space information for the disk
+  disk_usage = psutil.disk_usage('/')
+
+  # Format the data as a JSON object
+  json_body = [
+      {
+          "measurement": "disk_space",
+          "tags": {
+              "disk": "/"
+          },
+          "fields": {
+              "total": disk_usage.total,
+              "used": disk_usage.used,
+              "free": disk_usage.free,
+              "percent": disk_usage.percent
+          }
+      }
+  ]
+  return json_body
+
+def store_data(data):
+  # Write the data to the database
+  if influx.write_points(data) != True:
+    raise
+
+if __name__ == '__main__':
+  print('[Init application]: Disk monitoring.\n')
+  data = get_data()
+  store_data(data)
