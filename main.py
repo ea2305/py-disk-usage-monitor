83b90cf28e82de1ba109e4e7d0534febd42a5cfc
Elihu Cruz <eaca230596@gmail.com>
2022-12-21 20:12:16 -0600

    update insertion logic

--- main.py
+++ main.py
@@ -24,35 +24,28 @@ DB = {
 }
 
 # Setup
-influx = InfluxDBClient(DB['HOST'], DB['PORT'], DB['USER'], DB['PASSWORD'])
-dbs = influx.get_list_database()
-if DB['DATABASE'] not in dbs:
-  influx.create_database(DB['DATABASE'])
-
-influx.switch_database(DB['DATABASE'])
+influx = InfluxDBClient(DB['HOST'], DB['PORT'], DB['USER'], DB['PASSWORD'], DB['DATABASE'])
 
 def get_data_of_disk(disk, dtime):
   # Get the space information for the disk
   disk_usage = psutil.disk_usage(disk)
   
   # Format the data as a JSON object
-  json_body = [
-      {
-          "measurement": "disk_space",
-          "tags": {
-              "disk": disk
-          },
-          "fields": {
-              "total": disk_usage.total,
-              "used": disk_usage.used,
-              "free": disk_usage.free,
-              "percent": disk_usage.percent
-          },
-          "time": dtime
-      }
-  ]
+  data = {
+    "measurement": "disk_space",
+    "tags": {
+      "disk": disk
+    },
+    "fields": {
+      "total": disk_usage.total,
+      "used": disk_usage.used,
+      "free": disk_usage.free,
+      "percent": disk_usage.percent
+    },
+    "time": dtime
+  }
 
-  return json_body
+  return data
 
 def store_data(data):
   # Write the data to the database
@@ -67,11 +60,13 @@ if __name__ == '__main__':
     try:
       dt = datetime.now()
       dtime = int(datetime.timestamp(dt))
+      data = []
       for disk in CONFIG['DISK_PATHS']:  
-        data = get_data_of_disk(disk, dtime)
-        store_data(data)
+        record = get_data_of_disk(disk, dtime)
+        data.append(record)
 
-      print('[Data stored]')
+      store_data(data)
+      print(f'[Data stored] at: {dtime}')
       time.sleep(CONFIG['INTERVAL'])
     except Exception as err:
       logging.error(f'[error] saving disk path. {err}')
